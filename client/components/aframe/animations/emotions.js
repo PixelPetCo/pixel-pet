d87007845bc6dd2c5acc7dca813384cdc24a8608
Kẏra <kxra@riseup.net>
2018-05-04 14:38:03 -0400

    created a happy 'wag' animation

--- client/components/aframe/animations/emotions.js
+++ client/components/aframe/animations/emotions.js
@@ -2,15 +2,27 @@ import React from 'react'
 
 const happy = [
   {
-    name: 'animation 1',
+    name: 'wag',
     animations: {
-      head: [
+      body: [
         <a-animation
           key="1"
           attribute="rotation"
-          dur="10000"
+          dur="400"
+          direction="alternate"
           fill="forwards"
-          to="0 360 0"
+          from="0 10 45"
+          to="0 -10 45"
+          repeat="indefinite"
+        />,
+        <a-animation
+          key="2"
+          attribute="position"
+          dur="400"
+          direction="alternate"
+          fill="forwards"
+          from="-0.024 0.324 -1.501"
+          to="0.064 0.324 -1.501"
           repeat="indefinite"
         />
       ]
