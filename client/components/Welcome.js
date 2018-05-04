b443cd6a3113568f3a370553e1d1b96a9efdb385
elza-abraham <elzaeltho@gmail.com>
2018-05-03 19:45:28 -0700

    remove extra code

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -6,20 +6,21 @@ import Button from 'muicss/lib/react/button';
 
 // Welcome: a landing page for unauthenticated users
 const Welcome = (props) => (
-    !props.user.id ? (
-      <div>
-        <h1>Welcome to PixelPet!</h1>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Register</Link>
-          </li>
-        </ul>
+  !props.user.id ? (
+    <div>
+      <h1>Welcome to PixelPet!</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Register</Link>
+        </li>
+      </ul>
     </div>
-    )
+  )
     :
+    // Home: home/landing page for the authenticated users when logged in
     <main>
       <div id="translator">PixelPetSpeek Translator</div>
       <div id="a-frame">Companion component!</div>
@@ -30,21 +31,11 @@ const Welcome = (props) => (
         <button id="toggle-mic">MIC</button> */}
       </div>
     </main>
-    // <Link to="/home">
-    //     Home
-    //     </Link>
 )
 
 const mapState = ({ user }) => ({
   user
 })
 
-const mapDispatch = (dispatch, ownProps) => {
-  return {
-    redirectToHome: () => {
-      ownProps.history.push('/home')
-    }
-  }
-}
 
-export default connect(mapState, mapDispatch)(Welcome)
+export default connect(mapState, null)(Welcome)
