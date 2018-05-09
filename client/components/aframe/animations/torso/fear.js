222fb92d99656f2bbf2ee74dfefecd80eed852e1
Molly Hanes <mollyfhanes@gmail.com>
2018-05-09 14:50:12 -0500

    fear animation working

--- client/components/aframe/animations/torso/fear.js
+++ client/components/aframe/animations/torso/fear.js
@@ -4,11 +4,11 @@ const fear = [
   <a-animation
     key="1"
     attribute="position"
-    dur="150"
+    dur="90"
     direction="alternate"
     fill="forwards"
     from="0.024 0.324 -1.501"
-    to="0.03 0.324 -1.501"
+    to="0.018 0.324 -1.501"
     repeat="indefinite"
   />
 ]
