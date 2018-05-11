f0bfe97fd55bd02c501edb368f6db56d55892638
Kẏra <kxra@riseup.net>
2018-05-11 12:32:06 -0400

    fixed sitting head animation

--- client/components/aframe/animations/head/sit.js
+++ client/components/aframe/animations/head/sit.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const sit = [
   <a-animation
-    id="head-shake-angle"
+    id="head-sitting-angle"
     key="6"
     attribute="rotation"
     easing="linear"
@@ -10,10 +10,10 @@ const sit = [
     direction="alternate"
     fill="forwards"
     from="0 0 -45"
-    to="-45 0 -45"
+    to="45 -45 -45"
   />,
   <a-animation
-    id="head-shake-angle"
+    id="head-standing-angle"
     key="6"
     attribute="rotation"
     easing="linear"
@@ -21,11 +21,11 @@ const sit = [
     delay="4000"
     direction="alternate"
     fill="forwards"
-    from="0 0 -45"
-    to="-45 0 -45"
+    from="45 -45 -45"
+    to="0 0 -45"
   />,
   <a-animation
-    id="head-shake-height"
+    id="head-sit-height"
     key="5"
     attribute="position"
     dur="400"
@@ -33,8 +33,18 @@ const sit = [
     direction="alternate"
     fill="forwards"
     from="0.12789000425531563 0.12610999574468434 0.388331139133306"
+    to="0.02789000425531563 0.06610999574468434 0.388331139133306"
+  />,
+  <a-animation
+    id="head-sit-height"
+    key="5"
+    attribute="position"
+    dur="400"
+    delay="4000"
+    easing="linear"
+    fill="forwards"
+    from="0.12789000425531563 0.06610999574468434 0.388331139133306"
     to="0.02789000425531563 0.12610999574468434 0.388331139133306"
-    repeat="1"
   />
 ]
 
