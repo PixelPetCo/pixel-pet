bd4e4606ea4fd22e2d54a0ecf0ba40f98d8227c4
Kẏra <kxra@riseup.net>
2018-05-11 12:33:57 -0400

    modified torso sitting animation

--- client/components/aframe/animations/torso/sit.js
+++ client/components/aframe/animations/torso/sit.js
@@ -5,7 +5,6 @@ const joy = [
     key="1"
     attribute="rotation"
     dur="400"
-    direction="alternate"
     fill="forwards"
     from="0 0 45"
     to="-45 0 45"
@@ -15,7 +14,6 @@ const joy = [
     attribute="rotation"
     delay="4000"
     dur="400"
-    direction="alternate"
     fill="forwards"
     from="-45 0 45"
     to="0 0 45"
@@ -24,11 +22,18 @@ const joy = [
     key="3"
     attribute="position"
     dur="400"
-    direction="alternate"
     fill="forwards"
     from="0.024 0.324 -1.501"
+    to="0.024 0.224 -1.501"
+  />,
+  <a-animation
+    key="4"
+    attribute="position"
+    dur="400"
+    delay="4000"
+    fill="forwards"
+    from="0.024 0.224 -1.501"
     to="0.024 0.324 -1.501"
-    repeat="indefinite"
   />
 ]
 
