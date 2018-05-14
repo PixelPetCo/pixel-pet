6ef600b458b6c6ddae22eca9b345468048a98c29
Kẏra <kxra@riseup.net>
2018-05-14 11:34:14 -0400

    refactored remaining body part animations

--- client/components/aframe/animations/torso/joy.js
+++ client/components/aframe/animations/torso/joy.js
@@ -1,26 +1,22 @@
-import React from 'react'
-
 const joy = [
-  <a-animation
-    key="1"
-    attribute="rotation"
-    dur="400"
-    direction="alternate"
-    fill="forwards"
-    from="0 10 45"
-    to="0 -10 45"
-    repeat="indefinite"
-  />,
-  <a-animation
-    key="2"
-    attribute="position"
-    dur="400"
-    direction="alternate"
-    fill="forwards"
-    from="-0.024 0.324 -1.501"
-    to="0.064 0.324 -1.501"
-    repeat="indefinite"
-  />
+  {
+    attribute: 'rotation',
+    dur: '400',
+    direction: 'alternate',
+    fill: 'forwards',
+    from: '0 10 45',
+    to: '0 -10 45',
+    repeat: 'indefinite'
+  },
+  {
+    attribute: 'position',
+    dur: '400',
+    direction: 'alternate',
+    fill: 'forwards',
+    from: '-0.024 0.324 -1.501',
+    to: '0.064 0.324 -1.501',
+    repeat: 'indefinite'
+  }
 ]
 
 export default joy
