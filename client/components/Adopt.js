aca42e0d241873f531ca189bff33e78e2ee4d28b
Kẏra <kxra@riseup.net>
2018-05-14 14:45:11 -0400

    added adopt route

---
+++ client/components/Adopt.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { sendMessage } from '../store/chat'
+
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const styleText = {
+  position: 'absolute',
+  width: '18%',
+  right: '15%',
+  bottom: '14%'
+}
+
+const styleButton = {
+  position: 'absolute',
+  right: '9%',
+  bottom: '14%'
+}
+
+class MessageForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userMessage: ''
+    }
+  }
+
+  handleChange = evt => {
+    this.setState({
+      userMessage: evt.target.value
+    })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault()
+    this.props.sendUserMessage(this.state.userMessage)
+    this.setState({ userMessage: '' })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          type="text"
+          id="chatbox"
+          name="message"
+          hintText="Message to PixelPet..."
+          style={styleText}
+          value={this.state.userMessage}
+          onChange={this.handleChange}
+        />
+        <RaisedButton
+          type="submit"
+          label="send"
+          primary={true}
+          style={styleButton}
+        />
+      </form>
+    )
+  }
+}
+
+const mapDispatch = dispatch => ({
+  sendUserMessage: msg => dispatch(sendMessage(msg))
+})
+
+export default connect(null, mapDispatch)(MessageForm)
--- client/store/user.js
+++ client/store/user.js
@@ -35,7 +35,8 @@ export const auth = (credentials, method) => (
     .then(
       res => {
         dispatch(getUser(res.data))
-        history.push('/')
+        if (method === 'post') history.push('/adopt')
+        if (method === 'put') history.push('/')
       },
       authError => {
         // rare example: a good use case for parallel (non-catch) error handler
