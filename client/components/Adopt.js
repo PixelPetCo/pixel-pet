8a456c18be8b466481ea782b5a188baf65e08ce2
Kẏra <kxra@riseup.net>
2018-05-14 16:15:40 -0400

    basic form

--- client/components/Adopt.js
+++ client/components/Adopt.js
@@ -1,68 +1,121 @@
 import React, { Component } from 'react'
+import { editUser } from '../store/user'
 import { connect } from 'react-redux'
-import { sendMessage } from '../store/chat'
-
-import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 
-const styleText = {
-  position: 'absolute',
-  width: '18%',
-  right: '15%',
-  bottom: '14%'
-}
-
-const styleButton = {
-  position: 'absolute',
-  right: '9%',
-  bottom: '14%'
-}
-
-class MessageForm extends Component {
-  constructor() {
-    super()
-    this.state = {
-      userMessage: ''
-    }
+class Adopt extends Component {
+  state = {
+    petModel: '',
+    petPersonality: ''
   }
 
-  handleChange = evt => {
+  componentDidMount = () => {
+    const { user } = this.props
     this.setState({
-      userMessage: evt.target.value
+      petModel: user.petModel,
+      petPersonality: user.petPersonality
+    })
+  }
+
+  handleClick = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
     })
   }
 
   handleSubmit = evt => {
     evt.preventDefault()
-    this.props.sendUserMessage(this.state.userMessage)
+    this.props.editUser({
+      ...this.props.user,
+      petModel: this.state.petModel,
+      petPersonality: this.state.petPersonality
+    })
     this.setState({ userMessage: '' })
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <TextField
-          type="text"
-          id="chatbox"
-          name="message"
-          hintText="Message to PixelPet..."
-          style={styleText}
-          value={this.state.userMessage}
-          onChange={this.handleChange}
-        />
-        <RaisedButton
-          type="submit"
-          label="send"
-          primary={true}
-          style={styleButton}
-        />
+        <fieldset>
+          <legend>PixelPet Animal</legend>
+          <input
+            type="image"
+            onClick={this.handleClick}
+            name="petModel"
+            value="cuboidCanine"
+            alt="Cuboid Canine"
+            src="/images/cuboidCanine.png"
+            width="80"
+            height="80"
+          />
+          <input
+            type="image"
+            onClick={this.handleClick}
+            name="petModel"
+            value="triangleTurtle"
+            alt="Triangle Turtle"
+            src="/images/triangleTurtle.png"
+            width="80"
+            height="80"
+          />
+          <input
+            type="image"
+            onClick={this.handleClick}
+            name="petModel"
+            value="circleCat"
+            alt="Circle Cat"
+            src="/images/circleCat.png"
+            width="80"
+            height="80"
+          />
+          <input
+            type="image"
+            onClick={this.handleClick}
+            name="petModel"
+            value="boxyBunny"
+            alt="Boxy Bunny"
+            src="/images/boxyBunny.png"
+            width="80"
+            height="80"
+          />
+        </fieldset>
+        <fieldset>
+          <legend>PixelPet Personality</legend>
+          <ul>
+            <li>
+              <label htmlFor="chipper">
+                <RaisedButton
+                  onClick={this.handleClick}
+                  label="Chipper"
+                  name="petPersonality"
+                  value="chipper"
+                />
+              </label>
+            </li>
+            <li>
+              <label htmlFor="petPersonality">
+                <RaisedButton
+                  onClick={this.handleClick}
+                  label="Grumpy"
+                  name="petPersonality"
+                  value="grumpy"
+                />
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+        <RaisedButton type="submit" label="send" primary={true} />
       </form>
     )
   }
 }
 
 const mapDispatch = dispatch => ({
-  sendUserMessage: msg => dispatch(sendMessage(msg))
+  editUser: user => dispatch(editUser(user))
 })
 
-export default connect(null, mapDispatch)(MessageForm)
+const mapState = ({ user }) => ({
+  user
+})
+
+export default connect(mapState, mapDispatch)(Adopt)
