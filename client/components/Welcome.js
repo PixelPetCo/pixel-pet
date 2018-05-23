ffca198e58f0e4c18d31b7626e8ba922baa240cb
Molly Hanes <mollyfhanes@gmail.com>
2018-05-23 10:56:41 -0500

    hopefully fixed browser issues

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Landing from './Landing'
 import Home from './Home'
+import HomeNoSpeech from './HomeNoSpeech'
+import BrowserDetection from 'react-browser-detection'
+
+const browserHandler = {
+  chrome: () => <Home />,
+  default: () => <HomeNoSpeech />
+}
 
 // Welcome: a landing page for unauthenticated users
 const Welcome = props => {
@@ -9,7 +16,7 @@ const Welcome = props => {
     <Landing />
   ) : (
     // Home: home/landing page for the authenticated users when logged in
-    <Home />
+    <BrowserDetection>{browserHandler}</BrowserDetection>
   )
 }
 
