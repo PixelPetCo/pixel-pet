8ccbda9da7ca2cb295db7f96af5a2c63e6156536
Molly Hanes <mollyfhanes@gmail.com>
2018-06-06 15:02:44 -0500

    some final mobile tweaks

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -11,14 +11,12 @@ const browserHandler = {
   default: () => <HomeNoSpeech />
 }
 
-// Welcome: a landing page for unauthenticated users
 const Welcome = props => {
   return !props.user.id ? (
     <Landing />
   ) : !props.user.petModel ? (
     <Adopt />
   ) : (
-    // Home: home/landing page for the authenticated users when logged in
     <BrowserDetection>{browserHandler}</BrowserDetection>
   )
 }
